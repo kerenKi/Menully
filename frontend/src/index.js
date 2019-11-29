f3fd83c66994c9a840819aa71cee31bccf94cf93
kerenKi <kkinberg13@gmail.com>
2019-11-29 10:54:10 +0100

    Set up redux store and react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import store from './store'
+import {BrowserRouter} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
